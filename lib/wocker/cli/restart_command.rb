cd2906ef59f2aea8cd36d772df58d9917a9ca793
Matti Paksula <matti.paksula@iki.fi>
2018-12-16 20:14:00 +0200

    restart and login

--- lib/wocker/cli/restart_command.rb
+++ lib/wocker/cli/restart_command.rb
@@ -5,6 +5,12 @@ module Wocker
     class RestartCommand < Clamp::Command
       def execute
         Wocker::Vagrant.run "reload"
+        puts "sending control+alt+del"
+        `wocker keyboard key control alt del`
+        puts "writing vagrant"
+        `wocker keyboard write vagrant`
+        puts "pressing enter"
+        `wocker keyboard key enter`
       end
     end
   end
