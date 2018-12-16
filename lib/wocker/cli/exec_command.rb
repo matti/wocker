8cec0278f6a23802f83b80bedc676ae873f7ac70
Matti Paksula <matti.paksula@iki.fi>
2018-12-16 13:33:47 +0200

    exec workdir

--- lib/wocker/cli/exec_command.rb
+++ lib/wocker/cli/exec_command.rb
@@ -4,15 +4,12 @@ module Wocker
   module Cli
     class ExecCommand < Clamp::Command
       parameter "COMMAND ...", "command"
+      option "--workdir", "WORKDIR", "workdir"
 
       def execute
         command_list.insert 0, "cd"
-        workdir = if Wockerfile.workdir
-          Wockerfile.workdir
-        else
-          "/vagrant"
-        end
-        command_list.insert 1, "#{workdir};"
+        current_workdir = workdir || Wockerfile.workdir || "/vagrant"
+        command_list.insert 1, "#{current_workdir};"
 
         cmd = command_list.join " "
         Wocker::Vagrant.run "winrm", "-c", cmd
